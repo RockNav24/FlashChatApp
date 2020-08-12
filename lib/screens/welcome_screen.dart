bd0e4c19300e136df9287e9e0c95012cbc513008
navodsanthana <navodsanthana@gmail.com>
2020-08-12 01:37:00 +0100

    FlashChatApp working with firebase

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,95 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/componants/round_button.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String ID = "welcome_screen";
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _controller.forward();
+
+    animation.addStatusListener((status) {
+      print(status);
+    });
+
+    _controller.addListener(() {
+      print(_controller.value);
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var children2 = <Widget>[
+      Row(
+        children: <Widget>[
+          Hero(
+            tag: 'flash-logo',
+            child: Container(
+              child: Image.asset('images/logo.png'),
+              height: 60.0 * _controller.value,
+            ),
+          ),
+          TypewriterAnimatedTextKit(
+            speed: Duration(milliseconds: 500),
+            text: ['Flash Chat'],
+            textStyle: TextStyle(
+              fontSize: 35.0,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 48.0,
+      ),
+      RoundButton(
+        text: 'Log In',
+        color: Colors.lightBlueAccent,
+        function: () => Navigator.pushNamed(context, LoginScreen.ID),
+      ),
+      RoundButton(
+        text: 'Register',
+        color: Colors.blueAccent,
+        function: () => Navigator.pushNamed(context, RegistrationScreen.ID),
+      ),
+    ];
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(_controller.value),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: children2,
+        ),
+      ),
+    );
+  }
+}
